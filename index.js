12f9202b596fee2e4d2723c60bdc5cc38ab1172b
Anishka Chaudhari <anishkahc@gmail.com>
2019-04-03 20:01:05 +0530

    Add files via upload
    UI

---
+++ index.js
@@ -0,0 +1,25 @@
+﻿const summarizer = require('nodejs-text-summarizer')
+const translate = require('@vitalets/google-translate-api');
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.set('view engine', 'ejs');
+
+app.post('/', function (req, response) {
+translate(req.body.data, {client: 'gtx' ,to: 'en'}).then(res => {
+    let result = summarizer(res.text)
+    translate(result, {client: 'gtx' ,to: 'mr'}).then(res => {
+	response.render('index',{ data : res.text});
+    }).catch(err => {
+        console.error(err);
+    });     
+}).catch(err => {
+    console.error(err);
+}); 
+})
+
+
+
+var server=app.listen(3000,function() {});
